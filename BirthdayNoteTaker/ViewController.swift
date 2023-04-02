d4b7ac7bd03aa073414f9adf399991fe098a1246
Berk <berk77_@hotmail.com>
2023-04-02 19:02:12 +0300

    Initial Commit

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //Casting → as? vs as!(force casting)
+        
+        if let newName : String = storedName as? String
+        {
+            nameLabel.text = "Name: \(newName)"
+        }
+        
+        if let newBirthday : String = storedBirthday as? String
+        {
+            birthdayLabel.text = "Birthady: \(newBirthday)"
+        }
+        
     }
-
+    
+    
+    
+    @IBAction func deleteOnClick(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName as? String) != nil
+        {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name: "
+        }
+        
+        if (storedBirthday as? String) != nil
+        {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday: "
+        }
+        
+    }
+    
+    @IBAction func saveOnClick(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
 
 }
 
